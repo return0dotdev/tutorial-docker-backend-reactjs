ed96dd0f1ae541c0516520c4150deef377b3c397
return0 <phoommin.naksenee@gmail.com>
2022-11-06 13:36:02 +0700

    let's go front-end docker mysql tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 
-function App() {
+
+export default function App() {
+  const [items, setItems] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("http://localhost:3333/attractions").then((res) => res.json()).then((data) => {
+      setItems(data)
+      console.log(data)
+    }, (error) => {
+      console.log(error)
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography variant="h4" gutterBottom>
+            Around the world!
+          </Typography>
+          <Grid container spacing={2}>
+            {items?.data?.map((it, index) => (
+              <Grid item xs={12} sm={4} key={index}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={it.coverimage}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {it.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                     {it.detail}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Learn More</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
